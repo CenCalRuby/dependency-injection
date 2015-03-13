f6b7b3caf56766cd047fe2a44f49a2cb7870b501
Frank West <frank.west.iii@gmail.com>
2015-03-12 17:40:16 -0700

    Implemented DI change

--- lib/auditor.rb
+++ lib/auditor.rb
@@ -1,6 +1,10 @@
 class Auditor
+  def initialize(audit_file)
+    @audit_file = audit_file
+  end
+
   def audit(value)
-    File.open('audit.txt', 'a') do |w|
+    File.open(@audit_file, 'a') do |w|
       w.puts(value)
     end
   end
