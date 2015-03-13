eb7e70ce8c2ea1f3e5b3e1f8c5ac9de0d8f29719
Frank West <frank.west.iii@gmail.com>
2015-03-12 20:17:42 -0700

    Update accounting.rb

--- accounting.rb
+++ accounting.rb
@@ -1,7 +1,8 @@
 require_relative 'lib/account'
 
 if __FILE__ == $0
-  account = Account.new(ARGV[0].to_f, ARGV[1])
+  auditor = Auditor.new(ARGV[1])
+  account = Account.new(ARGV[0].to_f, auditor)
   puts account.balance
   account.deposit(50)
   puts account.balance
